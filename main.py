e596ed988991f25b49745990fba3b1b9fb31b1a0
koby <kobyisr1@gmail.com>
2024-11-04 14:06:45 +0200

    Create end point for hour and contact pages

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+app.mount("/static",StaticFiles(directory="static"),name="static")
+app.mount("/img",StaticFiles(directory="img"),name="img")
+
 templates = Jinja2Templates(directory="templates")
 
-@app.get('/Home/', response_class = HTMLResponse)
+@app.get("/Home/", response_class = HTMLResponse)
 def index(request: Request):
   context = {'request':request}
   return templates.TemplateResponse("index.html",context)
+
+@app.get("/Home/hours", response_class=HTMLResponse)  # Remove .html extension
+def hours(request: Request):
+    context = {'request': request}
+    return templates.TemplateResponse("hours.html", context)
+
+@app.get("/Home/contact", response_class=HTMLResponse)  # Remove .html extension
+def contact(request: Request):
+    context = {'request': request}
+    return templates.TemplateResponse("contact.html", context)
